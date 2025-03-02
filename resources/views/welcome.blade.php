a489c028bc87bd944a14d41240a92ec49ca7ea15
engineersohag <engineer.sohag.cse@gmail.com>
2025-03-02 15:09:08 +0600

    Login & Registation Option Done

--- resources/views/welcome.blade.php
+++
@@ -1,1 +0,0 @@
-<h2 class="bg-primary">Hello Bangladesh</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// for user page  
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+// for admin page 
+
+Route::middleware(['auth', AuthAdmin::class])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
