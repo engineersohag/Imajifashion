fa2dfbae9fa32cc139c70d3240c4382ea16746e6
engineersohag <engineer.sohag.cse@gmail.com>
2025-03-13 21:40:40 +0600

    Filtaring

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,13 @@ class ShopController extends Controller
         $o_column = "";
         $o_order = "";
         $order = $request->query('order') ? $request->query('order') : -1;
+
         $f_brands = $request->query('brands');
+        $f_categories = $request->query('categories');
+
+        $min_price = $request->query('min') ? $request->query('min') : 1;
+        $max_price = $request->query('max') ? $request->query('max') : 500;
+
         switch($order){
             case 1: 
                 $o_column = 'created_at';
@@ -37,10 +44,17 @@ class ShopController extends Controller
         }
 
         $brands = Brand::orderBy('name', 'ASC')->get();
+        $categories = Category::orderBy('name', 'ASC')->get();
+
         $products = Product::where(function($query) use ($f_brands){
             $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'".$f_brands."'=''");
+        })->where(function($query) use ($f_categories){
+            $query->whereIn('category_id', explode(',', $f_categories))->orWhereRaw("'".$f_categories."'=''");
+        })->where(function($query) use ($min_price, $max_price){
+            
         })->orderBy($o_column, $o_order)->paginate($size);
-        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands'));
+
+        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories', 'min_price', 'max_price'));
     }
 
     public function product_details($product_slug){
